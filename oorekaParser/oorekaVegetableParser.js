f2d235b7685a2f253bce37915164b6480a959991
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2020-05-01 17:04:11 +0200

    progress on vegetable parser

--- oorekaParser/oorekaVegetableParser.js
+++ oorekaParser/oorekaVegetableParser.js
@@ -1,13 +1,27 @@
-const { get } = require("../lib/https");
+const { scrap } = require("../lib/https");
 const htmlparser2 = require("htmlparser2");
 
+var show = false;
+var next = "";
+var tag = "";
 const parser = new htmlparser2.Parser(
   {
     onopentag(name, attribs) {
-      console.log(name, attribs);
+      tag = name;
     },
     ontext(text) {
-      console.log(text);
+      text = text.replace(/\s+/g, "");
+      if (text.length == 0) return;
+
+      if (show) {
+        console.log(next + text);
+        show = false;
+      }
+
+      if (text == "Famille" && tag == "p") {
+        show = true;
+        next = "Famille : ";
+      }
     },
     onclosetag(tagname) {},
   },
@@ -16,9 +30,9 @@ const parser = new htmlparser2.Parser(
 
 async function test() {
   console.log(
-    //parser.write(
-    await get("https://jardinage.ooreka.fr/plante/voir/178/bourrache")
-    // )
+    parser.write(
+      await scrap("https://jardinage.ooreka.fr/plante/voir/178/bourrache")
+    )
   );
   parser.end();
 }
