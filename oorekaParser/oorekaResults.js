60febc7c4d43fc6b56f57220629c2ca10cd3a665
matheoberger <matheo.berger@viacesi.fr>
2020-05-01 15:44:23 +0200

    add gitignore and triy to get from Ooreka

--- oorekaParser/oorekaResults.js
+++ oorekaParser/oorekaResults.js
@@ -1,1 +1,45 @@
-const get = require(".libhttps.js");
+const { get } = require("../lib/https.js");
+const htmlparser2 = require("htmlparser2");
+
+async function loadpage() {
+  const result = await get(
+    "https://jardinage.ooreka.fr/plante/recherche?motsClefs=bourrache"
+  );
+  console.log(`result of LoadPage  ${result}`);
+  //   const parse = new Promise(async (resolve) => {});
+  return null;
+}
+
+const parser = new htmlparser2.Parser(
+  {
+    onopentag(name, attribs) {
+      if (name === "script" && attribs.type === "text/javascript") {
+        console.log("JS! Hooray!");
+      }
+    },
+    ontext(text) {
+      console.log("-->", text);
+    },
+    onclosetag(tagname) {
+      if (tagname === "script") {
+        console.log("That's it?!");
+      }
+    },
+  },
+  { decodeEntities: true }
+);
+
+async function test() {
+  console.clear();
+  console.log(
+    parser.write(
+      await get(
+        "https://jardinage.ooreka.fr/plante/recherche?motsClefs=bourrache"
+      )
+    )
+  );
+  parser.end();
+}
+
+loadpage();
+// test();
