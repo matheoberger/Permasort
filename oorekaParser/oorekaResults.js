3f81650066b60205a669c4200cae21d97c316b92
matheoberger <matheo.berger@viacesi.fr>
2020-05-01 19:32:37 +0200

    update

--- oorekaParser/oorekaResults.js
+++ oorekaParser/oorekaResults.js
@@ -1,20 +1,21 @@
-const { get } = require("../lib/https.js");
+const { scrap } = require("../lib/https.js");
 const htmlparser2 = require("htmlparser2");
+const util = require("util");
 
-async function loadpage() {
-  const result = await get(
-    "https://jardinage.ooreka.fr/plante/recherche?motsClefs=bourrache"
-  );
-  console.log(`result of LoadPage  ${result}`);
-  //   const parse = new Promise(async (resolve) => {});
-  return null;
-}
+// async function loadpage() {
+//   const result = await get(
+//     "https://jardinage.ooreka.fr/plante/recherche?motsClefs=bourrache"
+//   );
+//   console.log(`result of LoadPage  ${result}`);
+//   //   const parse = new Promise(async (resolve) => {});
+//   return null;
+// }
 
 const parser = new htmlparser2.Parser(
   {
     onopentag(name, attribs) {
       if (name === "script" && attribs.type === "text/javascript") {
-        console.log("JS! Hooray!");
+        console.log(`tage is : ${name},${util.inspect(attribs)}`);
       }
     },
     ontext(text) {
@@ -29,17 +30,25 @@ const parser = new htmlparser2.Parser(
   { decodeEntities: true }
 );
 
-async function test() {
-  console.clear();
-  console.log(
-    parser.write(
-      await get(
-        "https://jardinage.ooreka.fr/plante/recherche?motsClefs=bourrache"
-      )
-    )
+function filterItems(arr, query) {
+  return arr.filter(
+    (el) => el.toLowerCase().indexOf(query.toLowerCase()) !== -1
   );
-  parser.end();
 }
 
-loadpage();
-// test();
+async function test() {
+  console.clear();
+  var result = parser.write(
+    await scrap(
+      "https://jardinage.ooreka.fr/plante/recherche?motsClefs=bourrache"
+    )
+  );
+
+  //   console.log(filterItems(result, "bourrache"));
+  console.log(result);
+  parser.end();
+  console.log("coucou");
+}
+
+// loadpage();
+test();
