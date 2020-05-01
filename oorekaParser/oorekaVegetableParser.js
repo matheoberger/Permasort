ce221791d60f46f45eeeda0bac6b9daaca36c6aa
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2020-05-01 15:33:27 +0200

    adding gitignore

--- oorekaParser/oorekaVegetableParser.js
+++ oorekaParser/oorekaVegetableParser.js
@@ -1,9 +1,26 @@
 const { get } = require("../lib/https");
+const htmlparser2 = require("htmlparser2");
+
+const parser = new htmlparser2.Parser(
+  {
+    onopentag(name, attribs) {
+      console.log(name, attribs);
+    },
+    ontext(text) {
+      console.log(text);
+    },
+    onclosetag(tagname) {},
+  },
+  { decodeEntities: true }
+);
 
 async function test() {
   console.log(
+    //parser.write(
     await get("https://jardinage.ooreka.fr/plante/voir/178/bourrache")
+    // )
   );
+  parser.end();
 }
 
 test();
