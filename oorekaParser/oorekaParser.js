24db3de07e9fd15772fd8bd80922f4e9044bb997
matheoberger <matheo.berger@viacesi.fr>
2020-05-03 15:20:05 +0200

    Revert "Fix density regex's scrap to match more result."
    This reverts commit 50e09278b75d0fd9825ff01282a33ca5a479e26d.

--- oorekaParser/oorekaParser.js
+++ oorekaParser/oorekaParser.js
@@ -1,21 +1,11 @@
 const oorekaResults = require("./oorekaResults");
 const oorekaVegetableParser = require("./oorekaVegetableParser");
-// var system = require("system");
 
-var vegetable;
-
-async function loadData() {
-  vegetable = process.argv[2];
-  // console.log(vegetable);
+async function loadData(vegetable) {
   const vegetableURL = await oorekaResults.load(vegetable);
   var results = await oorekaVegetableParser.loadDataFromPage(vegetableURL);
-  return new Promise((response) => {
-    console.log(results);
-    response(results);
-  });
 }
-loadData();
 
-module.exports = {
-  loadData: loadData,
-};
+loadData("carotte");
+
+module.exports = {};
