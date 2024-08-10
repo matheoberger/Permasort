1e8ee8741ca85c56ce2d6e9d378dbced535f8940
Mathéo Berger <matheo.berger@viacesi.fr>
2024-08-10 20:47:05 +0200

    added month sowing for vegetables and new model association plan in order to compose associations

--- permasite/models.py
+++ permasite/models.py
@@ -6,9 +6,19 @@ class Vegetable(models.Model):
         return self.name
     name = models.CharField(max_length=200)
     family = models.CharField(max_length=200)
-    ground_sowing = models.CharField(max_length=200, blank=True)
-    greenhouse_sowing = models.CharField(max_length=200, blank=True)
+    ground_sowing = models.JSONField(default=list)
+    greenhouse_sowing =models.JSONField(default=list)
 
+    def save(self, *args, **kwargs):
+        if isinstance(self.ground_sowing, list):
+            print("Ground sowing is a list")
+            valid_months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+            for month in self.ground_sowing:    
+                if month not in valid_months:
+                    raise ValueError("Invalid month in ground_sowing list")
+            super().save(*args, **kwargs)  # Call the "real" save() method.
+        else:
+            return
 
 class GoodAssociation(models.Model):
     def __str__(self):
@@ -23,3 +33,8 @@ class BadAssociation(models.Model):
     left_vegetable = models.ForeignKey(Vegetable, on_delete=models.CASCADE, related_name='bad_left_associations')
     right_vegetable = models.ForeignKey(Vegetable, on_delete=models.CASCADE, related_name='bad_right_associations')
     name = models.CharField(max_length=200, blank=True)
+
+class AssociationPlan(models.Model):
+    def __str__(self):
+        return self.name
+    shape=models.JSONField(default=list)
