3980c95bacae4c9a530ac546d0cc24ca05fae777
Mathéo Berger <matheo.berger@viacesi.fr>
2024-08-10 14:23:15 +0200

    Better looking simple website

--- permasite/urls.py
+++ permasite/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("search/", views.search, name="search"),
+    path("", views.home, name="home"),
 ]
--- permasite/views.py
+++ permasite/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from django.db.models import Q 
-from .models import Vegetable, GoodAssociation
+from .models import Vegetable, GoodAssociation, BadAssociation
 
 def index(request):
     return HttpResponse("Hello, world. You're at the permasite index.")
@@ -14,10 +14,22 @@ def get_good_associations(request):
 def search(request):
     if request.method == 'POST':
         searched = request.POST.get('searched_vegetable')
-        results = Vegetable.objects.filter(name__icontains=searched)
-        associated_vegetables = GoodAssociation.objects.filter(Q(left_vegetable__in=results) | Q(right_vegetable__in=results))
-        print("good associations : ", associated_vegetables)
-        context = {'results': results}
-        return render(request, "result.html", {'results': associated_vegetables})
+        result = Vegetable.objects.filter(name__iexact=searched)
+        if not result:
+            messages.error(request, "Le légume que vous cherchez n'existe pas.")
+            return render(request, "search.html") # Rediriger pour afficher le message
+        else :
+            good_associated_vegetables = GoodAssociation.objects.filter(Q(left_vegetable__in=result) | Q(right_vegetable__in=result))
+            bad_associated_vegetables = BadAssociation.objects.filter(Q(left_vegetable__in=result) | Q(right_vegetable__in=result))
+
+            good_pairs = [[i.left_vegetable,i.right_vegetable] for i in good_associated_vegetables]
+            bad_pairs = [[i.left_vegetable,i.right_vegetable] for i in bad_associated_vegetables]
+
+            print("good associations : ", good_associated_vegetables)
+            print("bad associations : ", bad_associated_vegetables)
+            return render(request, "result.html",  {'searched_vegetable_name': result[0], 'good_pairs': good_pairs, 'bad_pairs': bad_pairs})
 
     return render(request, "search.html")
+
+def home(request):
+    return render(request, "home.html")
